8ad79ef813c75ee16c4e31f9a5fee3f53a4b2f42
unknown <reman1999@outlook.com>
2023-09-23 11:37:32 +0200

    reverse debug to false

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -36,7 +36,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpevh4xz)3w)6xuq6kv7g&3^xf^)gr-n3&p#%9')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['recipehub-f3766f54da15.herokuapp.com','127.0.0.1']
 
