1cd2ecacc868f53ee9c2333375a377c5a1f3e9e7
unknown <reman1999@outlook.com>
2023-09-30 02:47:40 +0200

    added recipe

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -144,14 +144,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
 STATICFILES_DIRS=[
    BASE_DIR / 'recipes/static'
 ]
-# The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
